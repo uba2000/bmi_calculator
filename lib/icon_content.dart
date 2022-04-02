1ce83e771ff13f960bab3d1c348cbbd493e35f0e
uba2000 <ubanoel2000@gmail.com>
2022-04-02 23:44:59 +0100

    add slider

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-const labelTextStyle = TextStyle(
-  fontSize: 18.0,
-  color: Color(0xFF8D8E98),
-);
+import 'constants.dart';
 
 class IconContent extends StatelessWidget {
   IconContent({required this.label, @required this.icon});
@@ -26,7 +22,7 @@ class IconContent extends StatelessWidget {
           ),
           Text(
             label,
-            style: labelTextStyle,
+            style: kLabelTextStyle,
           ),
         ]);
   }
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,11 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'icon_content.dart';
 import 'reusable_card.dart';
-
-const bottomContainerHeight = 80.0;
-const activeCardColor = Color(0xFF1D1F33);
-const inactiveCardColor = Color(0xFF111328);
-const bottomContainerColor = Color(0xFFEB1555);
+import 'constants.dart';
 
 enum Gender {
   male,
@@ -21,6 +17,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender? selectedGender;
+  int height = 180;
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +29,7 @@ class _InputPageState extends State<InputPage> {
       ),
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Expanded(
               child: Row(
@@ -39,8 +37,8 @@ class _InputPageState extends State<InputPage> {
                   Expanded(
                     child: ReusableCard(
                       colour: selectedGender == Gender.male
-                          ? activeCardColor
-                          : inactiveCardColor,
+                          ? kActiveCardColor
+                          : kInkActiveCardColor,
                       tapGesture: () {
                         setState(() {
                           selectedGender = Gender.male;
@@ -55,8 +53,8 @@ class _InputPageState extends State<InputPage> {
                   Expanded(
                       child: ReusableCard(
                     colour: selectedGender == Gender.female
-                        ? activeCardColor
-                        : inactiveCardColor,
+                        ? kActiveCardColor
+                        : kInkActiveCardColor,
                     tapGesture: () {
                       setState(() {
                         selectedGender = Gender.female;
@@ -68,20 +66,67 @@ class _InputPageState extends State<InputPage> {
                 ],
               ),
             ),
-            Expanded(child: ReusableCard(colour: activeCardColor)),
+            Expanded(
+                child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'HEIGHT',
+                    style: kLabelTextStyle,
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: <Widget>[
+                        Text(
+                          height.toString(),
+                          style: kNumberTextStyle,
+                        ),
+                        Text(
+                          'cm',
+                          style: kLabelTextStyle,
+                        ),
+                      ]),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                        thumbColor: Color(0xFFEB1555),
+                        overlayColor: Color(0x1FEB1555),
+                        activeTrackColor: Colors.white,
+                        inactiveTrackColor: Color(0xFF8D8E98),
+                        thumbShape:
+                            RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape:
+                            RoundSliderOverlayShape(overlayRadius: 30.0)),
+                    child: Slider.adaptive(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            )),
             Expanded(
               child: Row(
                 children: <Widget>[
-                  Expanded(child: ReusableCard(colour: activeCardColor)),
-                  Expanded(child: ReusableCard(colour: activeCardColor)),
+                  Expanded(child: ReusableCard(colour: kActiveCardColor)),
+                  Expanded(child: ReusableCard(colour: kActiveCardColor)),
                 ],
               ),
             ),
             Container(
-              color: bottomContainerColor,
+              color: kBottomContainerColor,
               margin: EdgeInsets.only(top: 10.0),
               width: double.infinity,
-              height: bottomContainerHeight,
+              height: kBottomContainerHeight,
             ),
           ],
         ),
